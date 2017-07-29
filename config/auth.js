43ed9a7c9d250218270a5c96f0cd51cee18abc2a
iankit3 <a4ankit@outlook.com>
2017-07-29 19:51:56 +0530

    au

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     'google' : {
         'clientID'         : '221434635957-t7bn75t6bte44bdigobh0p5bi2f3vdrb.apps.googleusercontent.com',
         'clientSecret'     : 'VGo8OgIY42TndgkBySEWuPBv',
-        'callbackURL'      : 'http://localhost:9999/auth/google/callback'
+        'callbackURL'      : 'https://mailatnodemailer.herokuapp.com/partials/test.html'
     }
 
 }
