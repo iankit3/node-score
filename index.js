18afe30361045a44b9d092dbf613fffb23c042b3
iankit3 <a4ankit@outlook.com>
2017-07-29 22:11:40 +0530

    auth:log

--- index.js
+++ index.js
@@ -37,7 +37,12 @@ app.get("/", (req, res) => {
 })
 app.use("/api/", router);
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['openid', 'email', 'profile'] }));
+app.get('/auth/google', passport.authenticate('google', { 
+  scope: [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email'
+    ]
+ }));
 app.get('/auth/google/callback',
   passport.authenticate('google', {
     failureRedirect: '/login'
